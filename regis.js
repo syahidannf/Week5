25f7fd5fb71cb4aeec96e3e2b9861cddcdadc377
syahidannf <118803307+syahidannf@users.noreply.github.com>
2023-02-07 03:20:55 +0700

    first commit

---
+++ regis.js
@@ -0,0 +1,54 @@
+class Perhitungan {
+  constructor(data) {
+    this.data = data;
+  }
+  hitungTotalUmur() {
+    this.totalUmur = 0;
+    for (let i = 0; i < data.length; i++) {
+      this.totalUmur += Number(data[i].umur);
+    }
+  }
+  hitungTotalUangSaku() {
+    this.totalUangSaku = 0;
+    for (let i = 0; i < data.length; i++) {
+      this.totalUangSaku += Number(data[i].uangsaku);
+    }
+  }
+
+  asyncHitungTotalUmur = (data) => {
+    return new Promise((resolve, reject) => {
+      this.totalUmur = 0;
+      for (let i = 0; i < data.length; i++) {
+        this.totalUmur += Number(data[i].umur);
+      }
+      resolve(this.totalUmur);
+    });
+  };
+  asyncHitungTotalUangSaku = (data) => {
+    return new Promise((resolve, reject) => {
+      this.totalUangSaku = 0;
+      for (let i = 0; i < data.length; i++) {
+        this.totalUangSaku += Nunber(data[i].uangsaku);
+      }
+      resolve(this.totalUangSaku);
+    });
+  };
+  hitungAll() {
+    Promise.all([this.asyncHitungTotalUmur(this.data), this.asyncHitungTotalUangSaku(this.data)]).then((result) => {
+      for (let cek of result) {
+        console.log(cek);
+      }
+      console.log("done");
+    });
+  }
+}
+
+class Statis extends Perhitungan {
+  constructor(data) {
+    super(data);
+  }
+  outputData() {
+    console.log("Total Umur " + this.totalUmur);
+    console.log("TOtal UangSaku " + this.totalUangSaku);
+  }
+}
